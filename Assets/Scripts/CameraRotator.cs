5eb9e2b262266deb79aa6045efcfda2e17fd59d3
Gosch <dmeyer.private@gmail.com>
2024-11-09 14:35:25 +0100

    feat: Start screen rotation

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class CameraRotator : MonoBehaviour
+{
+    float speed = -10f;
+    bool isRotating = true;
+    // Define the fixed position and rotation for the camera
+    Vector3 fixedPosition = new Vector3(0, 0, 0);
+    Vector3 fixedRotation = new Vector3(0, 0, 0);
+
+    [SerializeField] GameObject panel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRotating)
+        {
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        }
+    }
+
+    public void StopRotation()
+    {
+        isRotating = false;
+        // Set the camera's position and rotation when the game starts
+        transform.SetPositionAndRotation(fixedPosition, Quaternion.Euler(fixedRotation));
+
+        if (panel != null)
+        {
+            panel.SetActive(false); // deactivate panel
+        }
+        else
+        {
+            Debug.LogWarning("Panel referece not set");
+        }
+    }
+}
