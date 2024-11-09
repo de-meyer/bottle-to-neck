a7900623f67cceb03ad5c15d00931dec2720cacb
Philipp Runge <p.runge@progani.com>
2024-11-09 16:36:46 +0100

    feat: create playing pitch scene and add timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+  public float duration = 3;
+  public TextMeshProUGUI timeText;
+  float timeRemaining = 3;
+  public bool timerIsRunning = false;
+
+  private void Start()
+  {
+    // Starts the timer automatically
+    Debug.Log("Timer with duration of " + duration + "s started.");
+    timerIsRunning = true;
+    timeRemaining = duration;
+    timeText.text = duration.ToString();
+  }
+
+  void Update()
+  {
+    if (timerIsRunning)
+    {
+      if (timeRemaining > 0)
+      {
+        timeRemaining -= Time.deltaTime;
+        // update text mash input each full second
+        if (timeRemaining % 1 < Time.deltaTime)
+        {
+          DisplayTime(timeRemaining);
+        }
+      }
+      else
+      {
+        Debug.Log("Time has run out!");
+        timeRemaining = 0;
+        timerIsRunning = false;
+      }
+    }
+  }
+
+  void DisplayTime(float timeToDisplay)
+  {
+    int seconds = Mathf.Max(Mathf.FloorToInt(timeToDisplay), 0);
+    string text = seconds == 0 ? "*toooot*" : seconds.ToString();
+    timeText.text = text;
+  }
+}
